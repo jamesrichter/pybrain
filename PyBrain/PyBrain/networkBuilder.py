5922665474498b7597fef956c7f6cb75733babc4
jamesrichter <jamesaugustrichter@gmail.com>
2016-03-02 19:47:04 -0700

    messing around with adding networks

---
+++ PyBrain/PyBrain/networkBuilder.py
@@ -0,0 +1,20 @@
+print "mucking around with pybrain"
+print "building a network..."
+
+import pybrain
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer
+from pybrain.structure import SoftmaxLayer
+
+net = buildNetwork(2, 3, 1)
+
+net.activate([2,1])
+net['in']
+net['hidden0']
+net['out']
+
+net = buildNetwork(2, 3, 1, hiddenclass=TanhLayer)
+net['hidden0']
+net = buildNetwork(2, 3, 2, hiddenclass=TanhLayer, outclass = SoftmaxLayer)
+net = buildNetwork(2,3,1, bias=True)
+net['bias']
